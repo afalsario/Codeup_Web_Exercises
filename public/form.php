b574110fd57a4b85c5ce65660e2e70419e5753f2
Ashley <a_falsario@yahoo.com>
2014-05-23 12:37:18 -0500

    added a question with checkboxes

--- public/form.php
+++ public/form.php
@@ -6,7 +6,9 @@
 <body>
 <?php
 	var_dump($_GET);
+	echo "<br>";
 	var_dump($_POST);
+	echo "<br>";
 ?>
 	<h2>User Login</h2>
 
@@ -48,6 +50,34 @@
 			</label>
 		</p>
 		<p>
+			<button type="submit">Submit</button>
+		</p>
+		</form>
+
+		<h2>Multiple Choice Test</h2>
+		<form method="POST">
+			<p>
+				Am I excited for the weekend?
+				<label for="1Yes">Yes<input type="radio" id="1Yes" name="1form"></label>
+				<label for="1No">No<input type="radio" id="1No" name="1form"></label>
+			</p>
+			<p>
+				Am I doing anything fun?
+				<label for="Yes">Yes<input type="radio" id="Yes" name="2form"></label>
+				<label for="No">No<input type="radio" id="No" name="2form"></label>
+			</p>
+			<p>
+				What are my favorite movie genres?
+
+				<label for="comedy">Comedy<input type="checkbox" id="comedy" name="movies[]" value="comedy">
+
+				<label for="thriller">Thriller<input type="checkbox" id="thriller" name="movies[]" value="thriller"></label>
+
+				<label for="action">Action<input type="checkbox" id="action" name="movies[]" value="action"></label>
+
+				<label for="romance">Romance<input type="checkbox" id="romance" name="movies[]" value="romance"></label>
+			</p>
+			<p>
 			<button type="submit">Submit</button>
 		</p>
 		</form>
