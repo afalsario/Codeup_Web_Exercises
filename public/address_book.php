6564e13743742677e16bcce6cff2acfd2124669e
Ashley <a_falsario@yahoo.com>
2014-06-02 13:42:58 -0500

    initial commit containing basic comments

---
+++ public/address_book.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+function to store new entries
+
+validate 5 required fields: name, address, city, state and zip
+
+display error if information is missing
+
+save information to a csv file
+
+
+display information
+
+form for name
+form for address
+form for city
+form for state
+form for zip
+form for phone
+
+
+
+</body>
+</html>
