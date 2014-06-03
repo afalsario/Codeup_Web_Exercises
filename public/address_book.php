9a673bf8709b2a703b137167407a2cb12b9d0540
Ashley <a_falsario@yahoo.com>
2014-06-03 10:54:49 -0500

    added forms and basic code to write to a csv file

--- public/address_book.php
+++ public/address_book.php
@@ -5,65 +5,106 @@
 	// echo "<h1>POST</h1>";
 	// var_dump($_POST);
 	// echo "<br>";
+
+$filename = "address_book.csv";
+$address_book = [];
+$address_book[] = $_POST;
+
 // function to store new entries
-
-$contacts = $_POST;
-
-foreach($contacts as $type => $value)
+function save_contact($contacts, $file)
 {
-	echo "<table><tr><td>{$value}</td></tr></table>";
+	if(is_writeable($file))
+	{
+		$handle = fopen($file, 'w');
+		foreach ($contacts as $fields)
+		{
+			fputcsv($handle, $fields);
+		}
+		fclose($handle);
+	}
 }
 
+$isValid = false;
+var_dump($_POST);
+if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip_code']))
+{
+	$isValid = true;
+	if(empty($_POST['phone_number']))
+	{
+		array_pop($_POST);
+	}
+}
 
-// validate 5 required fields: name, address, city, state and zip
+if($isValid)
+{
+	save_contact($address_book, $filename);
+}
 
 // display error if information is missing
-
-// save information to a csv file
-
-
-// display information
-
-
-
-
-
 ?>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
+<!-- 	address book table -->
+
+	<table border= 1>
+		<tr>
+			<th>Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th>
+		</tr>
+	<?
+		foreach($address_book as $contact)
+		{
+			foreach ($contact as $type => $value)
+			{
+				// validate 5 required fields: name, address, city, state and zip
+				if (empty($value))
+				{
+					echo "Please enter valid $type.<br>";
+					echo "<td>" . '' . "</td>";
+				}
+				else
+				{
+					echo "<td>{$value}</td>";
+				// save information to a csv file
+				}
+			}
+		}
+	?>
+	</table>
 	<!-- forms for last and first name -->
-	<form method="POST">
-	<label for="contact_name">Name </label>
-		<input type="text" id="contact_name" name="contact_name" value="Ashley Falsario">
+	<br>
+	<form method="POST" action='address_book.php'>
+
+	<label for="name">Name </label>
+		<input type="text" id="name" name="name" value="<? $isValid && $_POST['name'] ? $_POST['name'] : '';?>">
 	<br>
 
 	<!-- form for address -->
 		<label for="address">Address </label>
-		<input type="text" id="address" name="address" value="782 Crestway Drive">
+		<input type="text" id="address" name="address">
 	<br>
 
 	<!-- form for city -->
 		<label for="city">City </label>
-		<input type="text" id="city" name="city" value="San Antonio">
+		<input type="text" id="city" name="city">
 	<br>
 
 	<!-- form for state -->
 		<label for="state">State </label>
-		<input type="text" id="state" name="state" value="TX">
+		<input type="text" id="state" name="state">
 	<br>
 
 	<!-- form for zip -->
 		<label for="zip_code">Zip Code </label>
-		<input type="text" id="zip_code" name="zip_code" value="78239">
+		<input type="text" id="zip_code" name="zip_code">
 	<br>
 
 
 	<!-- form for phone -->
 		<label for="phone_number">Phone Number </label>
-		<input type="text" id="phone_number" name="phone_number" value="(210) 319-8669">
+		<input type="text" id="phone_number" name="phone_number">
 	<br>
 
 	<input type="submit">
