331002cebb1fc18c632363753c091ac2720c6158
Ashley <a_falsario@yahoo.com>
2014-06-19 10:57:38 -0500

    overriding parent construct

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -1,9 +1,26 @@
 <?
 
+// TODO: require Filestore class
 require('classes/filestore.php');
 
 class AddressDataStore extends Filestore
 {
+
+	function __construct($filename = '')
+	{
+		parent:: __construct($filename);
+		$this->filename = strtolower($filename);
+	}
+
+    function read_address_book()
+    {
+        // TODO: refactor to use new $this->read_csv() method
+    }
+
+    function write_address_book($addresses_array)
+    {
+        // TODO: refactor to use new write_csv() method
+    }
 
 }
 ?>
